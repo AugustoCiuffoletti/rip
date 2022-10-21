bf1f96258cef053c9985102394e071712d2a1039
Augusto Ciuffoletti <augusto.ciuffoletti@gmail.com>
2022-10-21 17:12:31 +0200

    revisione 2022

--- rip.py
+++ rip.py
@@ -1,37 +1,41 @@
-from collections import defaultdict
-
-# routing: la tabella di routing (locale)
-# r_from: il router da cui ricevo l'advertisement
-# advertisement: l'advertisement
-def merge(routing,r_from,advertisement):
-	# Ricopio la tabella locale nell'output
-	t_out=defaultdict(list)
-	for dest in advertisement.keys(): 
-		advertisement[dest]=advertisement[dest]+1
-	for dest in routing.keys(): 
-		t_out[dest]=routing[dest]
-	# Scansione della tabella in input
-	for dest in advertisement.keys():
+	# La funzione esegue l'algoritmo utilizzando i due parametri sulla
+	# tabella di routing locale definita come variabile globale 
+def BellmanFord(src,dv):
+	# src: il router da cui viene ricevuto il distance vector
+	# dv: il distance vector, un dizionario (chiave -> valore) con
+	# - chiave = sottorete di destinazione 
+	# - valore = metrica
+	global rt; # dichiaro globale la routing table
+	# Incremento di uno tutte le distanze nel distance vector	
+	for dest in dv.keys(): 
+		dv[dest]=dv[dest]+1
+	# Scansione del distance vector e aggiornamento routing table
+	for dest in dv.keys():
 		# Se la destinazione e' nota
-		if dest in t_out.keys():
+		if dest in rt.keys():
 			# e il prossimo router e' quello da cui ricevo la tabella, 
 			# aggiorno il costo
-			if t_out[dest][0] == r_from: t_out[dest]=[r_from,advertisement[dest]]
+			if rt[dest][0] == src: rt[dest]=[src,dv[dest]]
             # altrimenti, se il costo e' inferiore aggiorno il costo e il router
-			elif t_out[dest][1] > advertisement[dest]: 
-				t_out[dest]=[r_from,advertisement[dest]]
+			elif rt[dest][1] > dv[dest]: 
+				rt[dest]=[src,dv[dest]]
         # se invece la destinazione non e' nota aggiungo una nuova riga
-		else: t_out[dest]=[r_from,advertisement[dest]]
-	return t_out
+		else: rt[dest]=[src,dv[dest]]
+	return
 
 # Esempio sugli appunti
-tabella_routing={
+# rt = routing table (variabile globale) e' un dizionario (chiave -> valore) con
+# - chiave: il nome della sottorete di destinazione
+# - valore: la coppia [next-hop,metrica]
+rt={
  'n1':['A',1],
  'n2':['C',1],
  'n3':['A',3],
  'n4':['B',1],
  'n5':['B',2]}
 
-out = merge(tabella_routing,'C',{'n2':1,'n3':0,'n6':4})
+BellmanFord('C',{'n2':1,'n3':0,'n6':4})
+print(rt)
+# qui potete aggiungere altre invocazioni
  
 print(out)
